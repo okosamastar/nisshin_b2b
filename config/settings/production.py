19bbbf87e18b5a4a64f3e00d7285016572c9559d
okosamastar <okosama.star.inc@gmail.com>
2022-03-24 07:33:34 +0900

    update static files

--- config/settings/production.py
+++ config/settings/production.py
@@ -107,7 +107,7 @@ SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[nisshin_b2b]",
+    default="[日清製粉ウェルナ]",
 )
 
 # ADMIN
