39bc66e574c664c2acc3ef73c7c488cf261b6eed
okosamastar <okosama.star.inc@gmail.com>
2022-03-31 15:47:35 +0900

    remove exhibition url

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("contact/", views.contactView.as_view(), name="contact"),
     path("ec/", views.storeView.as_view(), name="stores"),
     path("ec/micstore/", views.micstoreView.as_view(), name="micstore"),
-    path("spring_2022/", views.spring_2022, name="spring_2022"),
+    # path("spring_2022/", views.spring_2022, name="spring_2022"),
     path("404/", views.custom_page_not_found),
 ]
