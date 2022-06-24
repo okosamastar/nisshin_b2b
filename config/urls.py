5aeee5c9f95df7a481c87c05dd243d65e333daa2
okosamastar <okosama.star.inc@gmail.com>
2022-06-24 19:53:16 +0900

    update admin pannel

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
+
 # from django.views.generic import TemplateView
 from django.views.generic import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("jet/", include("jet.urls", "jet")),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
