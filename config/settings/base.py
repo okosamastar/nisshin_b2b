234de01ab224c7ef959a5b655f419e8198c76e7d
okosamastar <okosama.star.inc@gmail.com>
2022-07-06 09:31:25 +0900

    fix production "debug" probrem

--- config/settings/base.py
+++ config/settings/base.py
@@ -88,6 +88,7 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
     "pages",
     "products",
+    "brands",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
